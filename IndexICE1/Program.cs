3d3586902db51242d0f82564ce2e099d74b70f03
Hannah Terblanche <126674584+Diana-Magus@users.noreply.github.com>
2024-07-25 15:21:26 +0200

    removed all code in program

--- IndexICE1/Program.cs
+++ IndexICE1/Program.cs
@@ -8,28 +8,7 @@ namespace IndexICE1
         static void Main(string[] args)
         {
             
-            Player player1 = new Player(10, 104, "DianaMagus22","Warlock", "Half Elf", "Astrid");
-
-            Console.WriteLine(player1[2]);
-            Console.WriteLine(player1[0]);
-            Console.WriteLine(player1[1]);
-            Console.WriteLine(player1[3]);
-            Console.WriteLine(player1[4]);
-
-            Console.WriteLine("");
-
-
-            Console.WriteLine("Player Tag: " + player1["playertag"]);
-
-            Console.WriteLine("Character level: " + player1["level"]);
-
-            Console.WriteLine("Character health: " + player1["health"]);
-
-            Console.WriteLine("Character class: " + player1["characterclass"]);
-
-            Console.WriteLine("Character race: " + player1["characterrace"]);
-
-            Console.WriteLine("Character name: " + player1["charactername"]);
+            
         }
     }
 }
