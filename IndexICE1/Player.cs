e308414952a469bc36b8df2a834c5b0dba0fc6d9
Hannah Terblanche <126674584+Diana-Magus@users.noreply.github.com>
2024-07-25 12:52:08 +0200

    Add project files.

---
+++ IndexICE1/Player.cs
@@ -0,0 +1,148 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IndexICE1
+{
+    internal class Player
+    {
+        int Health, Level;
+        string PlayerTag, CharacterClass, CharacterRace, CharacterName; //dont have direct acess to these varibles
+
+        public Player(int health, int level,
+                  string playerTag,
+                  string characterClass,
+                  string characterRace,
+                  string characterName)
+        {
+
+            Health = health;
+            Level = level;
+            PlayerTag = playerTag;
+            CharacterClass = characterClass;
+            CharacterRace = characterRace;
+            CharacterName = characterName;
+        }
+
+
+        public object this[int index]
+        {
+            get
+            {
+                switch (index)
+                {
+                    case 0:
+                        return Health;
+                    case 1:
+                        return Level;
+                    case 2:
+                        return PlayerTag;
+                    case 3:
+                        return CharacterClass;
+                    case 4:
+                        return CharacterRace;
+                    case 5:
+                        return CharacterName;
+                    default:
+                        return null;
+                }
+            }
+
+            set
+            {
+                switch (index)
+                {
+                    case 0:
+                        Health = (int)value;
+                        break;
+                    case 1:
+                        Level = (int)value;
+                        break;
+                    case 2:
+                        PlayerTag = (string)value;
+                        break;
+                    case 3:
+                        CharacterClass = (string)value;
+                        break;
+                    case 4:
+                        CharacterRace= (string)value;
+                        break;
+                    case 5:
+                        CharacterName =(string)value;
+                        break;
+                    default:
+                        throw new ArgumentException("Invalid index provided.");
+
+                }
+
+            }
+        }
+
+        public object this[string attrName]
+        {
+            get
+            {
+                if (attrName.ToLower().Equals("health"))
+                {
+                    return Health;
+                }
+                if (attrName.ToLower().Equals("level"))
+                {
+                    return Level;
+                }
+                if (attrName.ToLower().Equals("playertag"))
+                {
+                    return PlayerTag;
+                }
+                if (attrName.ToLower().Equals("characterclass"))
+                {
+                    return CharacterClass;
+                }
+                if (attrName.ToLower().Equals("characterrace"))
+                {
+                    return CharacterRace;
+                }
+                if (attrName.ToLower().Equals("charactername"))
+                {
+                    return CharacterName;
+                }
+                return null;
+            }
+
+            set
+            {
+                if (attrName.ToLower().Equals("health"))
+                {
+                    Health = (int)value;
+                }
+                if (attrName.ToLower().Equals("levle"))
+                {
+                    Level= (int)value;
+                }
+                if (attrName.ToLower().Equals("playertag"))
+                {
+                    PlayerTag = (string)value;
+                }
+                if (attrName.ToLower().Equals("characterclass"))
+                {
+                    CharacterClass= (string)value;
+                }
+                if (attrName.ToLower().Equals("characterrace"))
+                {
+                    CharacterRace = (string)value;
+                }
+                if (attrName.ToLower().Equals("charactername"))
+                {
+                    CharacterName = (string)value;
+                }
+
+            }
+
+
+
+        }
+    }
+}
+
---
+++ IndexICE1/Program.cs
@@ -0,0 +1,36 @@
+﻿// See https://aka.ms/new-console-template for more information
+using IndexICE1;
+
+namespace IndexICE1
+{
+    internal class Progam
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            Player player1 = new Player(10, 104, "DianaMagus22","Warlock", "Half Elf", "Astrid");
+
+            Console.WriteLine(player1[0]);
+            Console.WriteLine(player1[1]);
+            Console.WriteLine(player1[2]);
+            Console.WriteLine(player1[3]);
+            Console.WriteLine(player1[4]);
+
+
+            Console.WriteLine("Player Tag: " + player1["playertag"]);
+
+            Console.WriteLine("Character level: " + player1["level"]);
+
+            Console.WriteLine("Character health: " + player1["health"]);
+
+            Console.WriteLine("Character class: " + player1["characterclass"]);
+
+            Console.WriteLine("Character race: " + player1["characterrace"]);
+
+            Console.WriteLine("Character name: " + player1["charactername"]);
+        }
+    }
+}
+
+
+
